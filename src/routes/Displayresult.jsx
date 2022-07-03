239e7b78c4e71f0090d6312831ef01495c4cc020
FlyingGooseFace <65017180+FlyingGooseFace@users.noreply.github.com>
2022-07-03 18:48:33 +0800

    Add files via upload

---
+++ src/routes/Displayresult.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Result from '../components/Result';
+import Navbar from '../components/Navbar';
+
+const Displayresult = () => {
+  return (
+    <div>
+      <Navbar />
+      <Result />
+    </div>
+  );
+};
+
+export default Displayresult
