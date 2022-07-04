9e0412e6a076bd2db485200f9f4f4fc1e60576f8
Musyaffaq <Musyaffaq@users.noreply.github.com>
2022-07-04 23:59:18 +0800

    Update App.js to remove outdated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home';
 import Contact from './routes/Contact';
@@ -18,5 +17,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById("root"));
