0d6305787db6a5ede1842f3e753f8c34c35f9d66
Musyaffaq <Musyaffaq@users.noreply.github.com>
2022-07-12 20:10:01 +0800

    add code to allow saving of json file and reading it later

--- api/api.py
+++ api/api.py
@@ -1,6 +1,5 @@
-import time
-from flask import Flask, send_file
-from flask import send_file
+from flask import Flask, send_file, jsonify, request, session
+import json
 
 app = Flask(__name__)
 
@@ -9,3 +8,16 @@ app = Flask(__name__)
 def get_video():
     # reading the video file output
     return send_file("../src/assets/duck.mp4")
+
+@app.route('/url_route', methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'GET':
+        with open("temp_gym_data.json", "r") as read_file:
+            data = json.load(read_file)
+        return data
+    elif request.method == 'POST':
+        data = request.json
+        print(data)
+        with open("temp_gym_data.json", "w") as write_file:
+            json.dump(data, write_file, indent=4)
+        return data 
